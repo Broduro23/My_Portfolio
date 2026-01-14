9457c8035922751c2aaa0de29d28873263df4f4a
Br4vin <Joseph.Bravin@strathmore.edu>
2026-01-14 18:12:09 +0300

    Premature Homepage

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
     return (
@@ -27,6 +28,23 @@ const Sidebar = () => {
 
                     </NavLink>
                 </nav>
+                <ul>
+                    <li>
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com">
+                            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel ="norefferer" href="https://www.instagram.com">
+                            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="norefferer" href="https://www.github.com">
+                            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </>
     );
